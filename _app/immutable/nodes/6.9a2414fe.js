efa9917a2d018c75f2eb85fe5c73d086364088d2
radixbillboard <145974635+radixbillboard@users.noreply.github.com>
2023-10-06 21:13:27 +0200

    Updates

---
+++ _app/immutable/nodes/6.9a2414fe.js
@@ -0,0 +1,42 @@
+import{s as g,f as d,a as v,I as y,g as h,d as o,c as m,K as f,j as r,k as c,w as b,i as u,L as n}from"../chunks/scheduler.8fc040ae.js";import{S as w,i as x}from"../chunks/index.8aa995e8.js";function T(p){let t,s,e,l=`<div class="blocks-grid svelte-4gqi89"><div class="h-full bg-[#00000080] min-[1300px]:bg-transparent min-[1300px]:overflow-y-scroll"><div class="flex flex-col gap-10 p-8"><div><h1 class="title svelte-4gqi89">Radix Billboard Terms of Service</h1> <p class="paragraph svelte-4gqi89">This document outlines the Terms of Service governing the use of The Radix Billboard. By
+                        accessing or using the service provided by The Radix Billboard, you agree to comply with and be
+                        bound by the following terms. Please review them carefully. If You disagree with any part of the
+                        terms, you may not access the service.</p></div> <div><h2 class="heading svelte-4gqi89">User Conduct</h2> <p class="paragraph svelte-4gqi89">The Radix Billboard provides you a platform to showcase yourself, your business, or
+                        your services. While we endorse a culture of free expression, you are prohibited from displaying
+                        or linking to content that is illegal, violent, sexually explicit, promotes hate speech, or is
+                        otherwise objectionable, such as harassment, discrimination, or other harmful behaviors. Should
+                        your content be deemed inappropriate, we will remove your block&#39;s data and withhold your
+                        security deposit. Severe violations may result in a temporary ban preventing further use of your
+                        block.</p></div> <div><h2 class="heading svelte-4gqi89">Assurance of Legal Content Upload</h2> <p class="paragraph svelte-4gqi89">You hereby represent and warrant that you possess the legal right and authority for all content
+                        you upload to the site. You assure that such material does not infringe upon any third-party
+                        intellectual property rights, including, but not limited to, copyright, trademark, and other
+                        proprietary rights. By uploading content to the site, you agree to indemnify and hold harmless
+                        The Radix Billboard and its affiliates against all claims, losses, liabilities, expenses,
+                        damages and costs, including, without limitation, attorneys&#39; fees, arising from or relating in
+                        any way to the Content you upload, your use of the Site, or any violation of these terms, any
+                        law, or any rights of any third party.</p></div> <div><h2 class="heading svelte-4gqi89">Limitation of Liability</h2> <p class="paragraph svelte-4gqi89">The Radix Billboard will not be liable for any damages, losses, or issues arising from the use
+                        of this service, including but not limited to, damages related to copyright infringements by
+                        users, and financial damages arising out of the use of this website. The service is provided &quot;as
+                        is&quot; and &quot;as available,&quot; without any representations or warranties of any kind, either express or
+                        implied.</p> <p class="paragraph svelte-4gqi89">Furthermore, The Radix Billboard does not guarantee or endorse the accuracy, completeness, or
+                        reliability of any user-uploaded content and will not be liable for any damages, losses, or
+                        issues arising out of or in connection with such content. By using this service, you acknowledge
+                        and agree that The Radix Billboard bears no responsibility for the content you or others upload,
+                        and you assume all risks associated with the use of, reliance on, or exposure to such content.</p></div> <div><h2 class="heading svelte-4gqi89">Continued Service Availability</h2> <p class="paragraph svelte-4gqi89">The operation and availability of The Radix Billboard depend on the current state of the Radix
+                        network and its public gateway services. This encompasses, but is not limited to, transaction
+                        costing limits, transaction size limits, and the unrestricted availability of gateway query
+                        methods. While we will make every effort to maintain the site, we reserve the right to
+                        discontinue the service if unexpected changes to the Radix network or its gateway services
+                        render continued operation infeasible.</p></div> <div><h2 class="heading svelte-4gqi89">Transaction Costs</h2> <p class="paragraph svelte-4gqi89">By using The Radix Billboard, you acknowledge that you will incur transaction
+                        costs while interacting with the on-ledger components of The Radix Billboard. These costs are
+                        determined by the Radix network and are beyond our control. The Radix Billboard is not
+                        responsible for setting or modifying these fees, and you agree to cover them as part of your
+                        usage of our service.</p></div> <div><h2 class="heading svelte-4gqi89">Financial Disclaimer</h2> <p class="paragraph svelte-4gqi89">Purchasing blocks on The Radix Billboard does not constitute an investment and
+                        should not be construed as such. Block NFTs are distributed with the sole intent of
+                        granting their owners administrative access to pixel space on The Radix Billboard. They are not
+                        intended for trading or for use in other financial activities. The Radix Billboard disclaims all
+                        liability for any financial losses or damages incurred by you in relation to the use of the
+                        service, whether direct or indirect.</p></div> <div><h2 class="heading svelte-4gqi89">Changes to Terms of Service</h2> <p class="paragraph svelte-4gqi89">The Radix Billboard reserves the right to amend these Terms of Service at any
+                        time. Users will be notified of changes via website banners or other appropriate methods. By
+                        continuing to use the service after changes to the Terms of Service, you agree to be bound by
+                        the revised terms.</p></div></div></div></div>`;return{c(){t=d("meta"),s=v(),e=d("div"),e.innerHTML=l,this.h()},l(i){const a=y("svelte-crv507",document.head);t=h(a,"META",{name:!0,content:!0}),a.forEach(o),s=m(i),e=h(i,"DIV",{class:!0,style:!0,"data-svelte-h":!0}),f(e)!=="svelte-1yv3yf1"&&(e.innerHTML=l),this.h()},h(){document.title="The Radix Billboard",r(t,"name","description"),r(t,"content","The Radix Billboard"),r(e,"class","w-screen h-screen flex justify-center items-center bg-[url(/background.jpg)]"),c(e,"background-size","cover"),c(e,"background-position","top")},m(i,a){b(document.head,t),u(i,s,a),u(i,e,a)},p:n,i:n,o:n,d(i){i&&(o(s),o(e)),o(t)}}}class R extends w{constructor(t){super(),x(this,t,null,T,g,{})}}export{R as component};
